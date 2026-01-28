7dcdacb4b5ec8a59403e778ce777e2ad5e288d15
zeel <sundayy119@gmail.com>
2026-01-28 07:15:20 +0530

    connection with frontend

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ from core.config import Base, engine
 from app.auth.router import router as auth_router
 from app.billing.router import router as billing_router
 from app.signals.router import router as signals_router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Trading signals SaaS")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(signals_router, prefix="/signals", tags=["Signals"])
